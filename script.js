8bcbb3110cf39abbd6e4716742a8f67c0ce0949c
Arauco <arishel@gmail.com>
2019-12-13 13:45:01 -0800

    created psuedo code, established initial variables for local storage

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 console.log("Hello World!")
+
+/////////////////// Home page /////////////////////////
+// TODO: MDP Add kid 
+// TODO: Go to parent portal stats.html
+// TODO: Go to kids page kid.html
+// TODO: PARKING LOT Login 
+
+
+
+/////////////////// Stats/parents portal /////////////////
+// TODO: View kids balances AJS
+//// TODO: Pull local storage 
+var kid1Ballance;
+var kid2Ballance;
+// TODO: View graph/history 
+//// TODO: moment.js
+////// TODO: Create local storage of day usage history AJS
+var kid1MoneyHist = ["12-13-19: 15",35,180,];
+var kid2MoneyHist = [15,35,180,];
+var kid1ScreenHist = [15,35,180,];
+var kid2ScreenHist = [15,35,180,];
+// TODO: Add time/money AJS
+//// TODO: update Local storage
+
+// TODO: Payout request confirmation
+//// TODO: Update local storage (balance decrease, request = 0)
+
+
+
+
+/////////////////// Kids page /////////////////
+// TODO: Edit/customize styles LH
+///// TODO: modal popout 
+
+// TODO: Start/stop time AJS
+//// TODO: Update local storage (per minute)
+
+
+//// TODO: API Youtube LH
+////// TODO: MVP static video URL request
+////// TODO: MDP dynamic search 
+
+// TODO: Request money AJS
+//// TODO: Update local storage
+var kid1MoneyRequest;
+var kid2MoneyRequest;
+
+/////////////////// Modal edit form /////////////////
+// TODO: Change name, age 
+//// TODO: Local storage AJS
+var kid1Info = ("name", "age")
+var kid2Info = ("name", "age") 
+//// TODO: Display
+
+// TODO: Change theme style LH
+//// TODO: Trigger: button click 
+//// TODO: Store preference
+var kid1Theme;
+var kid2Theme; 
+
+//// TODO: Append class to divs
+
+
+// TODO: Change background LH
+//// TODO: API unsplash

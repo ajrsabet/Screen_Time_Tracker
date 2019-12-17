40965c23cbc5b76bee2c8983ed538d92f7a1658c
Liam Hayes <liamh1267@gmail.com>
2019-12-16 21:59:12 -0800

    added both charts, just needs data inputs

--- script.js
+++ script.js
@@ -75,6 +75,89 @@ function kid1RefreshBalances() {
 /////////////////// Stats/parents portal ////////////////
 
 // TODO: View graph/history 
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+		// insert dates/time for info to be displayed
+        labels: ['12/1', '12/8', '12/15', '12/22', '12/29'],
+        datasets: [{
+			label: 'Time/Money Consumed over Time',
+			// insert data values for appropriate dates/times 
+            data: [kid1MoneyHist, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+			borderWidth: 1,
+			maintainAspectRatio: true,
+			
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx = document.getElementById('myChart2');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['12/1', '12/8', '12/15', '12/22', '12/29'],
+        datasets: [{
+            label: '# of Votes',
+            data: [kid1MoneyHist, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+			borderWidth: 1,
+			maintainAspectRatio: true,
+			
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
 //// moment.js
 
 // Clock 
